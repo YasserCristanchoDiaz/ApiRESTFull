5e0e707cc3eee437bca185f56d556e5aa96d4e4b
YasserCristanchoDiaz <yasser.cristancho@uptc.edu.co>
2023-12-12 19:01:13 -0500

    cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
@@ -10,6 +11,7 @@ app.set('PORT',process.env.PORT || 3000 )
 
 //middlewares
 app.use(express.json())
+app.use(cors())
 
 app.use("/city", require('./routes/city'))
 app.use("/stadiums", require('./routes/stadiums'))
